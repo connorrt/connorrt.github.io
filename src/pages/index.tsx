065763c2947097ea7d88a096af94f497100db275
Connor Taylor <connortaylor@52incs-iMac.local>
2019-03-22 13:57:01 -0400

    scrolling buttons and layout changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import {graphql} from 'gatsby';
-import * as React from 'react';
-import * as styles from './Index.module.scss';
+import {graphql} from "gatsby";
+import * as React from "react";
+import * as styles from "./Index.module.scss";
 
 interface IndexPageProps {
     data: {
@@ -8,6 +8,8 @@ interface IndexPageProps {
             siteMetadata: {
                 name: string;
                 tagline: string;
+                github: string;
+                linkedin: string;
             },
         },
     };
@@ -19,10 +21,34 @@ export const indexPageQuery = graphql`
       siteMetadata {
         name
         tagline
+        github
+        linkedin
       }
     }
   }
 `;
+
+function scrollTop(){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    });
+}
+function scrollSocial(){
+    window.scrollTo({
+        top:window.innerHeight,
+        left:0,
+        behavior: "smooth"
+    });
+}
+function scrollOther(){
+    window.scrollTo({
+        top:2*window.innerHeight,
+        left:0,
+        behavior: "smooth"
+    });
+}
 
 export default class IndexPage extends React.Component<IndexPageProps, {}> {
 
@@ -30,13 +56,29 @@ export default class IndexPage extends React.Component<IndexPageProps, {}> {
         const {
             name,
             tagline,
+            github,
+            linkedin,
         } = this.props.data.site.siteMetadata;
-        console.log('test');
+        console.log("test");
         return (
+
             <div className={styles.Container}>
-                <h1>{name}</h1>
-                <p>{tagline}</p>
+                <div className={styles.Header}>
+                    <button onClick={scrollTop} className={styles.Button}>Top</button>
+                    <button onClick={scrollSocial} className={styles.Button}>Social</button>
+                    <button onClick={scrollOther} className={styles.Button}>Other</button>
+                </div>
+                <div className={styles.Title}>
+                    <h1>{name}</h1>
+                    <h2>{tagline}</h2>
+                </div>
+                <h1>{`Social`}</h1>
+                <div className={styles.Links}>
+                    <a href={github}>Github</a>
+                    <a href={linkedin}>LinkedIn</a>
+                </div>
             </div>
         );
     }
 }
+
